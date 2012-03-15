7a81b1896a28d2564cdeee117205485672e906cb
Matt West <matt.west@developercity.co.uk>
2012-03-15 20:19:56 +0000

    Implemented AssetHandler and Page Templating System

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"http"
 	"template"
+	"io/ioutil"
+	"json"
 )
 
 type Page struct {
-	Title 		string
+	Slug				string
+	Title 			string
+	Keywords		string
+	Description	string
 }
 
 const pagePath = len("/")
@@ -19,13 +24,12 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	p := &Page{Title: r.URL.Path[pagePath:]}
-
-	if p.Title == "" {
-		p.Title = "index"
+	slug := r.URL.Path[pagePath:]
+	if slug == "" {
+		slug = "index"
 	}
 
-	c, err := template.ParseFile("pages/" + p.Title + ".html")
+	c, err := template.ParseFile("pages/" + slug + ".html")
 	// Check that the page exists
 	if err != nil {
 		e, _ := template.ParseFile("errors/404.html")
@@ -35,12 +39,43 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	p := findPage(slug)
+
 	t.Execute(w, "Header", p)
 	c.Execute(w, nil)
 	t.Execute(w, "Footer", nil)
 }
 
+func assetHandler(w http.ResponseWriter, r *http.Request) {
+	assetFile := r.URL.Path[pagePath:]
+	http.ServeFile(w, r, assetFile)
+}
+
+func findPage(slug string)(page Page) {
+	pagesRaw, _ := ioutil.ReadFile("pages/pages.json")
+
+	var pages []Page
+	err := json.Unmarshal(pagesRaw, &pages)
+
+	if err != nil {
+		// Do Something
+	}
+
+	//log.Print(pages)
+	for i:=0;i<len(pages);i++ {
+		if pages[i].Slug == slug {
+			page.Slug = pages[i].Slug
+			page.Title = pages[i].Title
+			page.Keywords = pages[i].Keywords
+			page.Description = pages[i].Description
+		}
+	}
+
+	return page
+}
+
 func main() {
 	http.HandleFunc("/", pageHandler)
+	http.HandleFunc("/assets/", assetHandler)
 	http.ListenAndServe(":9980", nil)
 }
