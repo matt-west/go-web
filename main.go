6945ed72f543ad65d851142863582c1a65af3b05
Matt West <matt.west@developercity.co.uk>
2012-04-30 20:43:27 +0100

    Applied changes from GO-Blog, removed unneccesary blog related code

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"http"
-	"template"
+	"encoding/json"
 	"io/ioutil"
-	"json"
+	"log"
+	"net/http"
+	"text/template"
 )
+
+type Config struct {
+	URL         string
+	Title       string
+	Description string
+	Lang        string
+	Webmaster   string
+}
 
 type Page struct {
 	Slug        string
@@ -14,43 +23,86 @@ type Page struct {
 	Description string
 }
 
+type Sitemap struct {
+	Config *Config
+	Pages  []Page
+}
+
+const assetPath = len("/")
 const pagePath = len("/")
 
+// Config
+var config = new(Config)
+
+// Pages
 var pages = make(map[string]*Page)
+var pagesJSON []Page
 var pageTemplates = make(map[string]*template.Template)
-var layoutTemplates *template.Set
-var errorTemplates *template.Set
+
+// Templates
+var layoutTemplates *template.Template
+var errorTemplates *template.Template
+var sitemapTemplate *template.Template
+
+// Static Assets i.e. Favicons or Humans.txt
+var staticAssets = []string{"humans.txt", "favicon.ico"}
 
 // Init Function to Load Template Files and JSON Dict to Cache
 func init() {
-	// Parse Page JSON Dict
-	pagesRaw, _ := ioutil.ReadFile("pages/pages.json")
-	var pagesJSON []Page
+	log.Println("Loading Config")
+	loadConfig()
+
+	log.Println("Loading Templates")
+	loadTemplates()
+
+	log.Println("Loading Pages")
+	loadPages()
+}
+
+// Load the Config File (config/app.json)
+func loadConfig() {
+	configRaw, _ := ioutil.ReadFile("config/app.json")
+	err := json.Unmarshal(configRaw, config)
+
+	if err != nil {
+		panic("Could not parse config file!")
+	}
+}
+
+// Load Pages Dict and Templates
+func loadPages() {
+	pagesRaw, _ := ioutil.ReadFile("data/pages.json")
 	err := json.Unmarshal(pagesRaw, &pagesJSON)
 	if err != nil {
 		panic("Could not parse Pages JSON!")
 	}
 
-	// Put Pages into pages map
 	for i := 0; i < len(pagesJSON); i++ {
 		pages[pagesJSON[i].Slug] = &pagesJSON[i]
 	}
 
-	// Parse and Cache Page Templates
 	for _, tmpl := range pages {
-		t := template.Must(template.ParseFile("./pages/" + tmpl.Slug + ".html"))
+		t := template.Must(template.ParseFiles("./pages/" + tmpl.Slug + ".html"))
 		pageTemplates[tmpl.Slug] = t
 	}
+}
 
-	// Parse and Cache Layout Templates
-	layoutTemplates = template.SetMust(template.ParseSetFiles("templates.html"))
-
-	// Parse and Cache Error Templates
-	errorTemplates = template.SetMust(template.ParseSetFiles("./errors/404.html", "./errors/505.html"))
+// Load Layout and Error Templates
+func loadTemplates() {
+	layoutTemplates = template.Must(template.ParseFiles("./templates/layouts.html"))
+	errorTemplates = template.Must(template.ParseFiles("./templates/errors/404.html", "./templates/errors/505.html"))
+	sitemapTemplate = template.Must(template.ParseFiles("./templates/sitemap.xml"))
 }
 
 // Page Handler Constructs and Serves Pages
 func pageHandler(w http.ResponseWriter, r *http.Request) {
+	// Check to see if the request is after a static asset
+	for _, asset := range staticAssets {
+		if asset == r.URL.Path[1:] {
+			http.ServeFile(w, r, asset)
+			return
+		}
+	}
 
 	// Get the page slug, use 'index' if no slug is present
 	slug := r.URL.Path[pagePath:]
@@ -62,7 +114,7 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 	_, ok := pages[slug]
 	if !ok {
 		w.WriteHeader(http.StatusNotFound)
-		errorTemplates.Execute(w, "404", nil)
+		errorTemplates.ExecuteTemplate(w, "404", nil)
 		return
 	}
 
@@ -70,28 +122,42 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 	p := pages[slug]
 
 	// Header
-	layoutTemplates.Execute(w, "Header", p)
+	layoutTemplates.ExecuteTemplate(w, "Header", p)
 
 	// Page Template
 	err := pageTemplates[slug].Execute(w, nil)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		errorTemplates.Execute(w, "505", nil)
+		errorTemplates.ExecuteTemplate(w, "505", nil)
 		return
 	}
 
 	// Footer
-	layoutTemplates.Execute(w, "Footer", nil)
+	layoutTemplates.ExecuteTemplate(w, "Footer", nil)
 }
 
 // Asset Handler Serves CSS, JS and Images
 func assetHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, r.URL.Path[pagePath:])
+	http.ServeFile(w, r, r.URL.Path[assetPath:])
+}
+
+// Sitemaps
+func sitemapHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/xml")
+	sitemap := Sitemap{config, pagesJSON}
+	sitemapTemplate.Execute(w, sitemap)
 }
 
 // Starts Server and Routes Requests
 func main() {
-	http.HandleFunc("/", pageHandler)
+	log.Println("Starting: " + config.Title)
+
 	http.HandleFunc("/assets/", assetHandler)
-	http.ListenAndServe(":9980", nil)
+	http.HandleFunc("/sitemap", sitemapHandler)
+	http.HandleFunc("/", pageHandler)
+
+	err := http.ListenAndServe(":9981", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
